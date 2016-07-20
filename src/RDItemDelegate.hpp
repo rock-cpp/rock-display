5e6cd679162b37085d35e81d67041d0d6a5746de
Eike Externest <externes@uni-bremen.de>
2016-07-20 17:04:09 +0200

    Added QActions

--- src/RDItemDelegate.hpp
+++ src/RDItemDelegate.hpp
@@ -4,6 +4,7 @@
 #include <QStandardItem>
 #include <QTreeView>
 #include <QMouseEvent>
+#include <QAction>
 #include "TaskModel.hpp"
 #include "Types.hpp"
 
@@ -15,7 +16,6 @@ public:
         // Do nothing.
     }
 
-    // QAbstractItemDelegate override
     virtual bool editorEvent(QEvent* event, TaskModel* model,
         const QStyleOptionViewItem& option, const QModelIndex& index)
     {
@@ -37,15 +37,27 @@ public:
     }
 
 private:
-    void showContextMenu(QStandardItem* item, const QPoint& globalPos) {
+    void showContextMenu(QStandardItem* item, const QPoint& globalPos)
+    {
         QMenu menu;
 
         switch (item->type()) {
             case ItemType::TASK:
-                menu.addAction("Befehl");
+                {
+                QAction *act = menu.addAction("Activate");
+                QAction *sta = menu.addAction("Start");
+                QAction *sto = menu.addAction("Stop");
+                QAction *con = menu.addAction("Configure");
+
+                //connect(act, SIGNAL(triggered()), this, SLOT(activate()));
+                //connect(sta, SIGNAL(triggered()), this, SLOT(start()));
+                //connect(sto, SIGNAL(triggered()), this, SLOT(stop()));
+                //connect(con, SIGNAL(triggered()), this, SLOT(configure()));
+                }
                 break;
 
             case ItemType::PORT:
+                // for (b : bla) {}
                 menu.addAction("Widget");
                 break;
         }
