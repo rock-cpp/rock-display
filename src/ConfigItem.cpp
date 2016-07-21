3a75079c7b9419410a6328ac5fd21adabb46e8e5
Janosch Machowinski <Janosch.Machowinski@dfki.de>
2016-07-21 15:38:16 +0200

    Added ConfigItem, to display configurations

---
+++ src/ConfigItem.cpp
@@ -0,0 +1,79 @@
+#include "ConfigItem.hpp"
+
+ConfigItem::ConfigItem()
+{
+
+}
+
+QList< QStandardItem* > ConfigItem::getValue(const std::shared_ptr< libConfig::ConfigValue >& value)
+{
+    switch(value->getType())
+    {
+        case libConfig::ConfigValue::ARRAY:
+            return getArray(value);
+            break;
+        case libConfig::ConfigValue::COMPLEX:
+            return getComplex(value);
+            break;
+        case libConfig::ConfigValue::SIMPLE:
+            return getSimple(value);
+            break;
+    }
+    
+    std::cout << "ERROR" << std::endl;
+    
+    return {};
+}
+
+QList< QStandardItem* > ConfigItem::getComplex(const std::shared_ptr< libConfig::ConfigValue >& value)
+{
+    libConfig::ComplexConfigValue *cVal = static_cast<libConfig::ComplexConfigValue *>(value.get());
+    QStandardItem *topName = new QStandardItem(cVal->getName().c_str());
+    
+    for(auto &v : cVal->getValues())
+    {
+        auto list = getValue(v.second);
+//         list.insert(0, new QStandardItem(v.second->getName().c_str()));
+        topName->appendRow( list );
+    }
+    
+    return {topName};
+}
+
+QList< QStandardItem* > ConfigItem::getSimple(const std::shared_ptr< libConfig::ConfigValue >& value)
+{
+    libConfig::SimpleConfigValue *sVal = static_cast<libConfig::SimpleConfigValue *>(value.get());
+    
+    return {new QStandardItem(sVal->getName().c_str()), new QStandardItem(sVal->getValue().c_str())};
+}
+
+QList< QStandardItem* > ConfigItem::getArray(const std::shared_ptr< libConfig::ConfigValue >& value)
+{
+    libConfig::ArrayConfigValue *aVal = static_cast<libConfig::ArrayConfigValue *>(value.get());
+    QStandardItem *topName = new QStandardItem(aVal->getName().c_str());
+    
+    for(auto &v : aVal->getValues())
+    {
+        topName->appendRow(getValue(v));
+    }
+    
+    return {topName};
+}
+
+void ConfigItem::update(const std::shared_ptr< libConfig::ConfigValue >& value, QStandardItem* parent)
+{
+    parent->removeColumns(0, parent->columnCount());
+    parent->removeRows(0, parent->rowCount());
+
+    parent->appendRow(getValue(value));
+    
+    parent->setText("FOO");
+}
+
+// QList< QStandardItem* >  ConfigItem::update(std::shared_ptr< libConfig::ConfigValue >& value)
+// {
+//     removeColumns(0, columnCount());
+//     removeRows(0, rowCount());
+//     
+//     
+// }
---
+++ src/ConfigItem.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <QStandardItem>
+#include <lib_config/Configuration.hpp>
+
+class ConfigItem
+{
+    std::vector<QStandardItem *> items;
+public:
+    ConfigItem();
+    
+    QList<QStandardItem *> getValue(const std::shared_ptr< libConfig::ConfigValue >& value);
+    QList<QStandardItem *> getComplex(const std::shared_ptr< libConfig::ConfigValue >& value);
+    QList<QStandardItem *> getSimple(const std::shared_ptr< libConfig::ConfigValue >& value);
+    QList<QStandardItem *> getArray(const std::shared_ptr< libConfig::ConfigValue >& value);
+
+    
+    void update(const std::shared_ptr< libConfig::ConfigValue >& value, QStandardItem* parent);
+    
+};
+
+
