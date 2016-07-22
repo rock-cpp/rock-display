6a549e16d8a181a04a1cfe98884fed0871d04e94
Eike Externest <externes@uni-bremen.de>
2016-07-22 13:43:39 +0200

    fixed TypeItem::setType

--- src/TypedItem.cpp
+++ src/TypedItem.cpp
@@ -12,13 +12,13 @@ TypedItem::TypedItem() : _type(-1)
 
 void TypedItem::setType(int newType)
 {
-
+    _type = newType;
 }
 
 int TypedItem::type()
 {
     if(_type == -1)
         return QStandardItem::type();
-    
+
     return _type;
 }
