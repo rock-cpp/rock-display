accb8b226c77caa3a9289ca2aaa33db79996d993
Eike Externest <externes@uni-bremen.de>
2016-07-22 18:39:45 +0200

    merged

--- src/Main.cpp
+++ src/Main.cpp
@@ -47,8 +47,6 @@ int main(int argc, char** argv)
     vizkit3d::Vizkit3DWidget vizkit3dWidget;
     QStringList *plugins = vizkit3dWidget.getAvailablePlugins();
 
-
-
     for(QString &p : *plugins)
     {
         std::cout << "Plugin : " << p.toStdString() << std::endl;
