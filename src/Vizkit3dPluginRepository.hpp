bd86270c499c4689b60f3c308291ea59f6b43609
Arne Böckmann <arne.boeckmann@dfki.de>
2017-04-20 13:06:33 +0200

    Sort widget list

--- src/Vizkit3dPluginRepository.hpp
+++ src/Vizkit3dPluginRepository.hpp
@@ -2,6 +2,8 @@
 
 #include <map>
 #include <vizkit3d/Vizkit3DPlugin.hpp>
+#include <string>
+#include <algorithm>
 
 #include <QObject>
 
@@ -20,6 +22,25 @@ public:
     std::string pluginName;
     std::string typeName;
     QMetaMethod method;
+};
+
+
+/** A functor that can be used with std::sort to sort to sort PluginHandles */
+struct PluginHandleSortByPluginName
+{
+    bool ascending;
+    /** @p ascending If true sort ascending, otherwise sort descending*/
+    PluginHandleSortByPluginName(bool ascending = true) : ascending(ascending) {}
+    
+    inline bool operator() (const PluginHandle& a, const PluginHandle& b)
+    {
+        if(ascending)
+            return std::lexicographical_compare(a.pluginName.begin(), a.pluginName.end(),
+                                                b.pluginName.begin(), b.pluginName.end());
+        else
+            return std::lexicographical_compare(b.pluginName.begin(), b.pluginName.end(),
+                                                a.pluginName.begin(), a.pluginName.end());
+    }
 };
 
 
