518e89a8d674eb28bb4edd840423d60ed80de044
Manuel Meyer <manuel.meyer@dfki.de>
2017-05-06 00:35:47 +0200

    remove debug output.

--- src/ConfigItem.cpp
+++ src/ConfigItem.cpp
@@ -40,7 +40,6 @@ bool VisualizerAdapter::removeVisualizer(QObject* plugin)
     {
         if (it->second.plugin == plugin)
         {
-            std::cout << "removing visualizer " << it->first << std::endl;
             visualizers.erase(it);
             return true;
         }
